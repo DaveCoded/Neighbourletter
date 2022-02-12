6aa0b9a3d2bf52297937ff0838b1157024c0f6d8
Maggie Appleton <5599295+MaggieAppleton@users.noreply.github.com>
2022-02-12 17:23:28 +0000

    added icons and styles

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -2,12 +2,19 @@ import React from 'react';
 import styles from '../styles/Navbar.module.css';
 
 export default function Navbar() {
-    return (
-        <nav className={styles.main}>
-            <p>NeighbourLetter</p>
-            <div className={styles.profile}>
-            <img src="http://via.placeholder.com/30x30" alt="profile" />
-            <p>Gill Fisherman</p></div>
-        </nav>
-    );
+  return (
+    <nav className={styles.main}>
+      <p>NeighbourLetter</p>
+      <div className={styles.profile}>
+        <img
+          className={styles.profileImage}
+          src="http://via.placeholder.com/30x30"
+          alt="profile"
+          width="30px"
+          height="30px"
+        />
+        <p>Gill Fisherman</p>
+      </div>
+    </nav>
+  );
 }
--- client/pages/dashboard.js
+++ client/pages/dashboard.js
@@ -3,14 +3,14 @@ import SubmissionList from '../components/SubmissionList';
 import styles from '../styles/Dashboard.module.css';
 
 export default function Dashboard() {
-    // fetch all submissions and filter to show pending submissions
+  // fetch all submissions and filter to show pending submissions
 
-    return (
-        <main className={styles.main}>
-            <section className={styles.submissionsList}>
-                <h2>Community Submissions</h2>
-                <SubmissionList submissions={allData.submissions} />
-            </section>
-        </main>
-    )
+  return (
+    <main className={styles.main}>
+      <section className={styles.submissionsList}>
+        <h2 className={styles.sectionHeader}>Community Submissions</h2>
+        <SubmissionList submissions={allData.submissions} />
+      </section>
+    </main>
+  );
 }
